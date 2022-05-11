fb0836c92123f679b6c0538ed53fd9950496176e
Vivek <vivekkohli935@gmail.com>
2022-05-11 17:52:23 +0530

    Heapsort for both to sort in ascending and descending order

--- Practice Codes/Heap Sort.c
+++ Practice Codes/Heap Sort.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-void createHeap(int*, int);
+void createHeap(int*, int, int);
+void heapify(int*, int, int, int);
+void heapSort(int*, int, int);
+
 void printHeap(int*, int);
-void heapify(int*, int, int);
-void heapSort(int*, int);
 void swap(int*, int, int);
 
 
@@ -21,39 +22,64 @@ int main()
   //   arr[i] = t[i];
   // }
 
+  int c;
+
+  printf("Sort Ascending : (1/0) ");
+  scanf("%d", &c);
+
   printHeap(arr, s);
-  createHeap(arr, s);
-  heapSort(arr, s);
+  createHeap(arr, s, c);
+  printHeap(arr, s);
+  heapSort(arr, s, c);
   printHeap(arr, s);
   // free(arr);
 }
 
 
-void createHeap(int* arr, int s)
+void createHeap(int* arr, int s, int c)
 {
   int p = floor((s-1)/2.0)-1;
   for(int i = p; i >= 0; i--)
-    heapify(arr, i, s);
+    heapify(arr, i, s, c);
 }
 
 
-void heapify(int* arr, int p, int s)
+void heapify(int* arr, int p, int s, int c)
 {
   int l = (2*p)+1;
   int r = (2*p)+2;
 
-  int largest = p;
 
-  if(l < s && arr[l] >= arr[largest])
+  if(c)  // Ascending Order -- max heap is created
+  {
+    int largest = p;
+    if(l < s && arr[l] >= arr[largest])
     largest = l;
 
-  if(r < s && arr[r] >= arr[largest])
+    if(r < s && arr[r] >= arr[largest])
     largest = r;
 
-  if(largest != p)
+    if(largest != p)
+    {
+      swap(arr, p, largest);
+      heapify(arr, largest, s, c);
+    }
+  }
+  else  // Descending Order -- min heap is created
   {
-    swap(arr, p, largest);
-    heapify(arr, largest, s);
+    int smallest = p;
+
+    if(l < s && arr[l] <= arr[smallest])
+    smallest = l;
+
+    if(r < s && arr[r] <= arr[smallest])
+    smallest = r;
+
+    if(smallest != p)
+    {
+      swap(arr, p, smallest);
+      heapify(arr, smallest, s, c);
+    }
   }
 }
 
@@ -64,12 +90,12 @@ void swap(int* arr, int i, int j)
   arr[j] = temp;
 }
 
-void heapSort(int* arr, int s)
+void heapSort(int* arr, int s, int c)
 {
   for(int i = s-1; i >= 0; i--)
   {
     swap(arr, 0, i);
-    heapify(arr, 0, i);
+    heapify(arr, 0, i, c);
   }
 }
 
